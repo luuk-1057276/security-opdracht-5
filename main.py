0080e8fa1c9ea33b6fbcf485bfd1425d4b7e8d67
luuk-1057276 <1057276@hr.nl>
2024-10-04 18:10:54 +0200

    added decrypt page. message and key are now stored in session

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__, template_folder='templates')
-
+app.secret_key = 'secret_key'
 
 @app.route('/', methods=['GET', 'POST'])
 def encrypt():
-    message = request.form.get('message')
-    key = request.form.get('key')
-    print(message)
-    print(key)
+    session['message'] = request.form.get('message')
+    session['key'] = request.form.get('key')
     return render_template('encrypt.html')
 
 @app.route('/decrypt')
 def decrypt():
-    return 'Hello, World!'
+    print(session['message'])
+    print(session['key'])
+    return render_template('decrypt.html', message = session['message'], key = session['key'])
 
 if __name__ == '__main__':
     app.run(debug=True)
