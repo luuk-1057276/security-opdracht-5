a2b3e9e56fdb7e24a46f883f4a3c11d334d26508
luuk-1057276 <1057276@hr.nl>
2024-10-10 15:09:06 +0200

    messages can now be encrypted and decrypted with a key

--- main.py
+++ main.py
@@ -1,18 +1,45 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect
+from cryptography.fernet import Fernet
+import base64
+import hashlib
 
 app = Flask(__name__, template_folder='templates')
 app.secret_key = 'secret_key'
 
+def get_key(key):
+
+    hashed_key = hashlib.sha256(key.encode()).digest()
+    return base64.urlsafe_b64encode(hashed_key)
+
 @app.route('/', methods=['GET', 'POST'])
 def encrypt():
-    session['message'] = request.form.get('message')
-    session['key'] = request.form.get('key')
+    message = request.form.get('message')
+    key = request.form.get('key')
+
+    if message and key:
+        hashed_key = get_key(key) 
+        cipher = Fernet(hashed_key)
+
+        session['message'] = cipher.encrypt(message.encode())
+        session['key'] = hashed_key
+        return redirect('/decrypt')
+
     return render_template('encrypt.html')
 
-@app.route('/decrypt')
+@app.route('/decrypt', methods=['GET', 'POST'])
 def decrypt():
-    print(session['message'])
-    print(session['key'])
+
+    key = request.form.get('key')
+    if key:
+        try:
+            hashed_key = get_key(key) 
+            cipher = Fernet(hashed_key)
+
+            session['message'] = cipher.decrypt(session['message']).decode()
+            return redirect('/decrypt')
+        except:
+            return render_template('decrypt.html', message = session['message'], key = session['key'])
+
     return render_template('decrypt.html', message = session['message'], key = session['key'])
 
 if __name__ == '__main__':
